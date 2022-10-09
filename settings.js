03c8d65af6eed20109c75eb233ada155747d0a08
przewnu <115214438+przewnu@users.noreply.github.com>
2022-10-09 21:21:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,7 +32,7 @@ module.exports = {
  ******************************************************************************/
 
     /** The file containing the flows. If not set, defaults to flows_<hostname>.json **/
-    flowFile: 'main_flow_v96.json',
+    flowFile: 'flows.json',
 
     /** By default, credentials are encrypted in storage using a generated key. To
      * specify your own secret, set the following property.
